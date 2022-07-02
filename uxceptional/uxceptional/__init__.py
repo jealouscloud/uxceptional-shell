821bacf198cdce256a15438a6fb9039f9ce5ab99
Noah Ablaseau <noaha@inmotionhosting.com>
2022-07-02 13:08:14 -0400

    Provide class exports

--- uxceptional/uxceptional/__init__.py
+++ uxceptional/uxceptional/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '0.1.0'
-from .windowbase import WindowBase
-from .shellwindow import ShellWindow
+from .windowbase import WindowBase, DataFetcher
+from .shellwindow import ShellWindow, Direction, MonitorPreference
 from .backend import Backend
 from . import apputils
