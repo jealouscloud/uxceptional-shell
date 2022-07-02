da5afb3ef674c86b4a7edf55b0089bfc546a529c
Noah Ablaseau <noaha@inmotionhosting.com>
2022-07-02 12:25:24 -0400

    Clean up init

--- uxceptional/uxceptional/__init__.py
+++ uxceptional/uxceptional/__init__.py
@@ -1,2 +1,5 @@
 __version__ = '0.1.0'
-from . import apputils, backend, shellwindow, windowbase
+from .windowbase import WindowBase
+from .shellwindow import ShellWindow
+from .backend import Backend
+from . import apputils
